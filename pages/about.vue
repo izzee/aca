0a74f989e1a631336560f85053dba8513e630887
Isaac Chabon <ichabon@me.com>
2024-07-02 12:57:15 -0400

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,12 @@
+<template>
+  <ContentDoc v-slot="{ doc }">
+    <main>
+      <h1>About</h1>
+      <p v-for="line in doc.intro">{{line.line}}</p>
+      <div v-for="person in doc.team">
+        <p>{{person.name}}</p>
+        <p>{{person.title}}</p>
+      </div>
+    </main>
+  </ContentDoc>
+</template>
